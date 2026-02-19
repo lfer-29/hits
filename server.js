3196c974082ccd10321ef0e3ea96a39669475e00
lfer-29 <lfermin@caldwell.edu>
2026-02-18 20:02:00 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express .static('public'));
+const hits = {
+    home: 0,
+    page1: 0,
+    page2: 0
+};  
+app.use(express.static('public'));
+app.get('/hits/:page', (req,res)=>{
+  const page = req.params.page;
+  hits[page]++;
+  res.json({count: hits[page]});
+});
+app.listen(3000);
+
